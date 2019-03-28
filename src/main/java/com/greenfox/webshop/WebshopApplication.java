173fbe2adee236c5727b78ac48c743b16d6e4f60
Diana <dianafbernardo@gmail.com>
2019-03-28 15:11:39 +0100

    Removed frontend

--- src/main/java/com/greenfox/webshop/WebshopApplication.java
+++ src/main/java/com/greenfox/webshop/WebshopApplication.java
@@ -1,7 +1,5 @@
 package com.greenfox.webshop;
 
-import com.greenfox.webshop.model.Product;
-import com.greenfox.webshop.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,22 +8,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class WebshopApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)   {
     SpringApplication.run(WebshopApplication.class, args);
-  }
-
-
-  @Bean
-  CommandLineRunner runner(ProductService productService) {
-    return args -> {
-      productService.save(new Product(1L, "TV Set", 300.00, "http://placehold.it/200x100"));
-      productService.save(new Product(2L, "Game Console", 200.00, "http://placehold.it/200x100"));
-      productService.save(new Product(3L, "Sofa", 100.00, "http://placehold.it/200x100"));
-      productService.save(new Product(4L, "Icecream", 5.00, "http://placehold.it/200x100"));
-      productService.save(new Product(5L, "Beer", 3.00, "http://placehold.it/200x100"));
-      productService.save(new Product(6L, "Phone", 500.00, "http://placehold.it/200x100"));
-      productService.save(new Product(7L, "Watch", 30.00, "http://placehold.it/200x100"));
-    };
   }
 
 }
