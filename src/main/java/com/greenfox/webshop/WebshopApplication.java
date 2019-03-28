69153a07affb870e48a75c0bc28e1a5ee2cc36e1
Peter Vadasz <vadaszpeter3@gmail.com>
2019-03-28 14:13:52 +0100

    seeded db

--- src/main/java/com/greenfox/webshop/WebshopApplication.java
+++ src/main/java/com/greenfox/webshop/WebshopApplication.java
@@ -4,6 +4,7 @@ import com.greenfox.webshop.model.Book;
 import com.greenfox.webshop.model.Order;
 import com.greenfox.webshop.model.OrderItem;
 import com.greenfox.webshop.model.User;
+import com.greenfox.webshop.repository.BookRepository;
 import com.greenfox.webshop.repository.OrderItemRepository;
 import com.greenfox.webshop.repository.OrderRepository;
 import com.greenfox.webshop.repository.UserRepository;
@@ -22,6 +23,8 @@ public class WebshopApplication implements CommandLineRunner {
   UserRepository userRepository;
   @Autowired
   OrderItemRepository orderItemRepository;
+  @Autowired
+  BookRepository bookRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(WebshopApplication.class, args);
@@ -34,6 +37,7 @@ public class WebshopApplication implements CommandLineRunner {
     OrderItem orderItem1 = new OrderItem(5, book1);
     User user1 = new User("a", "b");
     Order order1 = new Order(Arrays.asList(orderItem1), user1, Order.Status.PROCESSED);
+    bookRepository.save(book1);
     orderItemRepository.save(orderItem1);
     userRepository.save(user1);
     orderRepository.save(order1);
